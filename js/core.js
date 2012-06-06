ae2599a8bbbe2bc09e50a9b83c3e0b1d5d282fee
Kevin Q. Yu <kevin@w3softwares.com>
2012-06-06 16:25:22 -0700

    Debug

--- js/core.js
+++ js/core.js
@@ -466,6 +466,7 @@ W3S.Core.Event.Handler = {
                 }
                 var conf = {'dataType':'json','beforeSubmit':W3S.Core.Ajax.beforeSubmit,'success':W3S.Core.Ajax.success};
                 if (form.find('input[type="file"]').length>0) conf['dataType'] = 'html';
+console.log(trigger);
                 if (form.is('.w3s-ajax')&&(form.data('events')===W3S.Core.Constant.undefined||form.data('events').submit.length<1)) {
 					// for the form haven't bind submit event
                     if (jQuery().ajaxForm) {    // check plugin first
